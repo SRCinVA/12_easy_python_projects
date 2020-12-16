4bfea9f53071e25140fdae147f6870043e13f10c
SRCinVA <scitarella@hotmail.com>
2020-12-16 14:34:32 -0500

    Madlibs

---
+++ madibs.py
@@ -0,0 +1,12 @@
+person = "Kenobi"
+print(f"Well, hullo there, {person}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! \
+I also enjoy {verb1}. Go ahead and {verb2} like {famous_person}."
+
+print(madlib)
