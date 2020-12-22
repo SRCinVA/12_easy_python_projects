9c1d0cfb24aa8c9728b0483ac0309b3822e5c401
SRCinVA <scitarella@hotmail.com>
2020-12-22 13:29:53 -0500

    Finished hangman

--- hangman.py
+++ hangman.py
@@ -21,13 +21,39 @@ def hangman():
     alphabet = set(string.ascii_uppercase) # this imports a pre-existing list of upper case Latin letters.
     used_letters = set() # what the user has guessed.
 
-    # getting user input
-    user_letter = input("Guess a letter: ").upper()
-    if user_letter in alphabet - used_letters:   # Amazing. Didn't know you could subtract sets in Python.
-        used_letters.add(user_letter)
-        if user_letter in word_letters: # if that letter has already been used, you wouldn't get here.
-            word_letters.remove(user_letter)
+    lives = 6
 
+    # getting user input
+    while len(word_letters > 0) and lives > 0:
+        # to help out your users, print out the letters they've already used
+        print("You have ", lives, " left and you've already used these letters: ', '".join(used_letters)) # .join iterates over an iterable and turns them into a string.
+
+        # tell them what the current word is, with dashes indicating unguessed letters.
+        word_list = [letter if letter in used_letters else '-' for letter in word]  
+        print('Current word: ', ' '.join(word_list)) # not sure what's going on with this one.
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:   # Amazing. Didn't know you could subtract sets in Python.
+            used_letters.add(user_letter)   # if you guess a letter, and it's still an option in the remaining alphabet, it gets added to used_letters.
+            if user_letter in word_letters: # if that letter is already depleted, you wouldn't even get to the inner if statement. NOW it tests against word_letters.
+                word_letters.remove(user_letter) # if that letter is present in word_letters, it's removed (and you get it right).
+
+            else:
+                lives = lives - 1
+                print('Sorry, that letter\'s not in the word.')
+
+        elif user_letter in used_letter:
+            print("you've already used this one, dog. Try again.")
+
+        else:
+            print("This is an invalid character.")
+
+    if lives == 0:
+        print("Game over, bruh.")
+    else:
+        ("You guessed the word", word, "!!")
+
+    # gets here when len(word_letters) == 0
 
 user_input = input("Type something: ")
 print(user_input)
